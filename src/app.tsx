50a6b25339980dc355aaca8f5794ecf97306860e
Naelson <naelso.nuness@gmail.com>
2023-11-23 12:01:27 -0300

    add: add ffmpeg

---
+++ src/app.tsx
@@ -0,0 +1,105 @@
+import { Github, Wand2 } from "lucide-react";
+import { Button } from "./components/ui/button";
+import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
+import { Separator } from "./components/ui/separator";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
+import { Slider } from "./components/ui/slider";
+import { VideoInputForm } from "./components/video-input-form";
+
+export function App() {
+  return (
+    <div className="min-h-screen flex flex-col ">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">Desenvolvido com Amor </span>
+          
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" /> Github
+          </Button>
+        </div>
+      </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea 
+              className="resize-none p-4 leading-relaxed" 
+              placeholder="Inclua o prompt para a IA..."
+            />
+
+            <Textarea 
+              className="" 
+              placeholder="Resultado da IA"
+              readOnly
+            />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se vc pode utilizar a variavel <code className="text-violet-400">{'transcription'} </code> no seu prompt para alterar a transcrição do vídeo selecionado.
+          </p>
+        </div>
+        <aside className="w-88 space-y-6">
+          <VideoInputForm />
+          <Separator />
+
+          <form className="space-y-6">
+          <div className="space-y-2">
+              <Label>Prompt</Label>
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="selecione um prompt" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="titulo">Título</SelectItem> 
+                  <SelectItem value="descricao">Descrição</SelectItem> 
+                </SelectContent>
+              </Select>
+
+            </div>
+
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+              <Select disabled defaultValue="gpt3.5">
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5</SelectItem> 
+                </SelectContent>
+              </Select>
+              <span className="block text-xs text-muted-foreground italic">
+                Você poderá customizar essa opção em breve
+              </span>
+            </div>
+
+            <Separator />
+
+            <div className="space-y-2">
+               
+            <Label>Temperatura</Label>
+
+              <Slider 
+                min={0}
+                max={1}
+                step={0.1}
+              />
+              <span className="block text-xs text-muted-foreground italic leading-relaxed">
+                Valores mais altos tendem a deixar o resultaso mais criativo e com possíveis erros
+              </span> 
+            </div>
+
+            <Separator />
+
+            <Button type="submit" className="w-full">
+              Executar
+              <Wand2 className="w-4 h-4 ml-2"/>
+            </Button>
+          </form>
+        </aside>
+      </main>
+    </div>
+  )
+}
